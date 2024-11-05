83236e2bcecdc3e75f8f095fff116945148cb362
ALFIYA A <alfiyazon@gmail.com>
2024-11-05 12:34:26 +0530

    5-10-2024

---
+++ 221 fn ret.py
@@ -0,0 +1,8 @@
+#Return values
+#To let a function return a value,use the return statement
+
+def my_func(x):
+    return 5*x
+print(my_func(4))
+print(my_func(5))
+print(my_func(3))
---
+++ 223 fn sqr.py
@@ -0,0 +1,15 @@
+#1-Calculate the square of a number and return the result
+# using return statement
+
+def find_square(num):
+    return num*num
+
+print(find_square(5))
+print(find_square(4))
+print(find_square(7))
+
+#2-Concatenate two strings and return the result using return statement
+
+def concat2strings(str1,str2):
+    return str1+" "+str2
+print(concat2strings("Hi","Dear"))
